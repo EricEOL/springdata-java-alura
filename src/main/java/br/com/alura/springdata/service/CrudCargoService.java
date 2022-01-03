0b23d5ff181540d3d7f3b4bf0e794e4919e7832d
EricEOL <lopes.eric051@gmail.com>
2022-01-03 12:06:47 -0300

    Atualizando dados do db com o método save()

--- src/main/java/br/com/alura/springdata/service/CrudCargoService.java
+++ src/main/java/br/com/alura/springdata/service/CrudCargoService.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 @Service
 public class CrudCargoService {
 
+    private Boolean system = true;
+
     private final CargoRepository repository;
 
     public CrudCargoService(CargoRepository repository) {
@@ -16,7 +18,29 @@ public class CrudCargoService {
     }
 
     public void inicial(Scanner scanner) {
-        salvar(scanner);
+        while (system) {
+            System.out.println("Qual ação de cargo você deseja?");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Salvar");
+            System.out.println("2 - Atualizar");
+
+            int action = Integer.parseInt(scanner.nextLine());
+
+            switch (action) {
+                case 2:
+                    atualizar(scanner);
+                    break;
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 0:
+                    system = false;
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+        }
     }
 
     private void salvar(Scanner scanner) {
@@ -28,6 +52,20 @@ public class CrudCargoService {
 
         repository.save(cargo);
         System.out.println("Cargo salvo.");
+    }
+
+    private void atualizar(Scanner scanner) {
+        System.out.println("Qual o id do cargo ?");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Descrição do Cargo:");
+        String descricao = scanner.nextLine();
+
+        Cargo cargo = new Cargo();
+        cargo.setId(id);
+        cargo.setDescricao(descricao);
+
+        repository.save(cargo);
+        System.out.println("Cargo: " + id + " | atualizado.");
     }
 
 }
